80b82a14669587815f25dd3256385677f755ceb7
TShoreGit <tommyshore@live.co.uk>
2024-03-04 22:50:46 +0000

    Added colour choices, bug fixes to assigning troop amount, strength amount in printing cards fix

--- Ph1Methods.py
+++ Ph1Methods.py
@@ -12,20 +12,18 @@ def player_select(): #Gathering and accepting amount of Players
 def player_alloc(): #Assigning base values to players
     players = []
     amount_of_players = player_select()
+    colours = ["Red", "Green", "Yellow", "Purple", "Orange", "Blue"]
     for i in range(amount_of_players):
-        players.append(Player)
-        if amount_of_players == 2:
-            players[i].troop_amount = 40
-        elif amount_of_players == 3:
-            players[i].troop_amount = 35
-        elif amount_of_players == 4:
-            players[i].troop_amount = 30
-        elif amount_of_players == 5:
-            players[i].troop_amount = 25
-        else:
-            players[i].troop_amount = 20
-        players[i].cards_amount = 0
-        players[i].territories = []
+        colour_pick_pass = False
+        while colour_pick_pass == False:
+            colour_pick = input(f"Player {i + 1} enter your colour from {colours}: ")
+            if colour_pick in colours:
+                colours.remove(colour_pick)
+                colour_pick_pass = True
+            else:
+                print("Invalid colour chosen")
+        players.append(Player((50) - (5 * amount_of_players), 0, [], colour_pick))
+        #print(f"Player {i + 1}, Troop Amount: {players[i].troop_amount}, Colour: {players[i].colour}")
 
 def get_territories():
     territories = Territories()
@@ -35,5 +33,4 @@ def get_territories():
 def print_all_cards():
     cards = Cards()
     for card in cards.get_cards():
-        print(f"Territory: {card.territory.name}, Army Type: {card.army_type.name}")
-print_all_cards()
+        print(f"Territory: {card.territory.name}, Army Type: {card.army_type.name} Strength: {card.army_type.strength}")
