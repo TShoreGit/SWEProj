e65185e162bf688682dde54a609c21b7bb87695a
TShoreGit <tommyshore@live.co.uk>
2024-04-29 02:46:09 +0100

    Added dev mode, strengthen territories, player turns, borders, territory powers, assign armies & general overhaul

--- Ph1Methods.py
+++ Ph1Methods.py
@@ -1,13 +1,39 @@
+import sys
+import math
+
 from skClasses import *
 def player_select():  # Gathering and accepting amount of Players
     accepted = False
     while not accepted:
         amount_of_players = int(input("Enter amount of players (2-6): "))
-        if amount_of_players > 6 or amount_of_players < 2:
+        if amount_of_players == 9:
+            dev_mode()
+            sys.exit(0)
+        elif amount_of_players > 6 or amount_of_players < 2:
             print("Invalid amount of players")
         else:
             accepted = True
     return amount_of_players
+
+
+def strengthen_territories(players, player_turns_index, strengthen_amount):
+    for player_turn_index in player_turns_index:
+        accepted = False
+        while not accepted:
+            territory_choice = input(
+                f"Press (0) for owned territories\nPlayer {player_turn_index + 1} enter territory to strengthen: ")
+            if territory_choice != "0":
+                territory_choice_index = players[player_turn_index].find_player_territory_index(territory_choice)
+                if territory_choice_index is not None:
+                    players[player_turn_index].territories[territory_choice_index].power += strengthen_amount
+                    players[player_turn_index].troop_amount -= strengthen_amount
+                    print(territory_choice, "strengthened to",
+                          players[player_turn_index].territories[territory_choice_index].power)
+                    accepted = True
+                else:
+                    print("Invalid territory")
+            else:
+                display_owned_territories(players, player_turns_index)
 
 
 def player_alloc():  # Assigning base values to players
@@ -25,36 +51,112 @@ def player_alloc():  # Assigning base values to players
             else:
                 print("Invalid colour chosen")
         dice_rolls.append(Attack_dice(0, 0, 0).roll_one_dice())
-        players.append(Player(50 - (5 * amount_of_players), 0, [], colour_choice))
+        players.append(Player(50 - (5 * amount_of_players), [], [], colour_choice))
         # print(f"Player {i + 1}, Troop Amount: {players[i].troop_amount}, Colour: {players[i].colour}")
     [print(f"Player {i + 1} rolls a {dice_rolls[i]}") for i in range(amount_of_players)]
     territories_init = Territories()
     player_turns_index = sorted(range(amount_of_players), key=lambda x: -dice_rolls[x])
-    while territories_init.get_territories():
+    if amount_of_players != 2:
+        while any(player.troop_amount > 0 for player in players):
+            while territories_init.get_territories():
+                for player_index in player_turns_index:
+                    print(f"Player {player_index + 1} places")
+                    pass_territory(territories_init, player_index, players)
+            print("All territories have been chosen")
+            strengthen_territories(players, player_turns_index)
+
+    # else:
+    # while territories_init.get_territories() - 14:
+
+
+def dev_mode():
+    territories_init = Dev_territories()
+    players = [Player(2, [], [], "Red"),
+               Player(2, [], [], "Blue"),
+               Player(2, [], [], "Green")]
+    player_turns_index = [0, 1, 2]
+    while any(player.troop_amount > 0 for player in players):
+        while territories_init.get_territories():
+            for player_index in player_turns_index:
+                print(f"Player {player_index + 1} places")
+                pass_territory(territories_init, player_index, players)
+        print("All territories have been chosen")
+        strengthen_territories(players, player_turns_index, 1)
+    turns = 1
+    while len(players) > 1:
+        print(f"\n---TURN {turns}---")
         for player_index in player_turns_index:
-            print(f"Player {player_index + 1} places")
-            pass_territory(territories_init, player_index, players)
-    print("All territories have been chosen")
+            assign_armies(players, player_index)
+            player_turn(players, player_index)
+        turns += 1
 
 
 def get_territories(territories):
-    for territory in territories.get_territories()[:10]:
+    for territory in territories.get_territories():
         print(f"Name: {territory.name}, Code: {territory.code}, Continent: {territory.continent}")
+
 
 def print_all_cards(cards):
     for card in cards.get_cards():
         print(f"Territory: {card.territory.name}, Army Type: {card.army_type.name} Strength: {card.army_type.strength}")
 
+
 def pass_territory(territories_init, player_turn_index, players):
     accepted = False
     while not accepted:
-        territory_choice = input("Enter territory to place army on: ")
-        territory_index = territories_init.find_index_by_name(territory_choice)
-        if territory_index >= 0:
-            accepted = True
+        territory_choice = input(
+            f"Press (0) for remaining territories\nPlayer {player_turn_index + 1} enter territory to place army on: ")
+        if territory_choice != "0":
+            territory_index = territories_init.find_index_by_name(territory_choice)
+            if territory_index >= 0:
+                accepted = True
+            else:
+                print("Invalid territory")
         else:
-            print("Invalid territory")
+            get_territories(territories_init)
     players[player_turn_index].territories.append(territories_init.territories[territory_index])
+    players[player_turn_index].territories[-1].power += 1
+    players[player_turn_index].troop_amount -= 1
+    # print(players[player_turn_index].territories[-1].name, "bordering territories are:")
+    # print(players[player_turn_index].territories[-1].bordering)
+    # print("TEST 1")
+    # print(territories_init.find_territory_by_name(territory_choice).get_bordering_territories())
+    # print("TEST 2")
+    # print(players[player_turn_index].territories[-1].get_bordering_territories())
     del territories_init.territories[territory_index]
-    print(f"Player {player_turn_index + 1}'s territories are: " + "\n".join([players[player_turn_index].territories[i].name for i in range(len(players[player_turn_index].territories))]))
+    print(f"Player {player_turn_index + 1}'s territories are: " + "\n".join(
+        [players[player_turn_index].territories[i].name for i in range(len(players[player_turn_index].territories))]))
+
+
+def assign_armies(players, player_index):
+    player = players[player_index]
+    armies_received = math.trunc(len(player.territories) / 3)  # Ensure minimum army count of 3
+    armies_received += player.all_continents_check_dev()  # Add bonus based on continents
+    armies_received = max(armies_received, 3)
+    print(f"Player {player_index + 1}, receives {armies_received} armies")
+    players[player_index].troop_amount += armies_received
+
+
+def display_owned_territories(players, player_index):
+    print(f"Player {player_index + 1}, you currently occupy:")
+    for territory in players[player_index].territories:
+        print(f"{territory.name}, Power: {territory.power}")
+
+
+def assign_cards(player, cards_init):
+    player.cards.append(cards_init.cards.pop(0))
+
+
+def player_turn(players, player_index):
+    display_owned_territories(players, player_index)
+    print(f"You have {players[player_index].troop_amount} armies you must deploy")
+    deploy_amount = 0
+    while deploy_amount < players[player_index].troop_amount:
+        deploy_amount = int(input("Enter amount to deploy: "))
+        if deploy_amount > players[player_index].troop_amount:
+            print("Invalid amount")
+        else:
+            strengthen_territories(players, player_index, deploy_amount)
+
+
 player_alloc()
--- skClasses.py
+++ skClasses.py
@@ -1,5 +1,8 @@
 import random
+
 random.seed(52)
+
+
 class World_map:
     def __init__(self, board_layout, country_name, continent_bonus, continent, borders):
         self.board_layout = board_layout  # array
@@ -8,17 +11,56 @@ class World_map:
         self.continent = continent  # array
         self.borders = borders  # boolean
 
+
 class Player:
-    def __init__(self, troop_amount, cards_amount, territories, colour):
-        self.troop_amount = troop_amount  # int
-        self.cards_amount = cards_amount  # int
-        self.territories = territories  # array
-        self.colour = colour  # string
+    def __init__(self, troop_amount, cards, territories, colour):
+        self.troop_amount = troop_amount
+        self.cards = cards
+        self.territories = territories  # This should be a list of Territory instances
+        self.colour = colour
+
+    def all_continents_check(self):
+        continent_counts = {}
+        for territory in self.territories:
+            continent_counts[territory.continent] = continent_counts.get(territory.continent, 0) + 1
+
+        bonus = 0
+        continent_requirements = {"North America": 9, "South America": 4, "Europe": 7, "Africa": 6, "Asia": 12,
+                                  "Australia": 4}
+        continent_bonuses = {"North America": 5, "South America": 2, "Europe": 5, "Africa": 3, "Asia": 7,
+                             "Australia": 2}
+        for continent, count in continent_counts.items():
+            if count == continent_requirements.get(continent, 0):
+                bonus += continent_bonuses.get(continent, 0)
+        return bonus
+
+    def all_continents_check_dev(self):
+        continent_counts = {}
+        for territory in self.territories:
+            continent_counts[territory.continent] = continent_counts.get(territory.continent, 0) + 1
+
+        bonus = 0
+        continent_requirements = {"North America": 1, "South America": 1, "Europe": 1, "Africa": 1, "Asia": 1,
+                                  "Australia": 1}
+        continent_bonuses = {"North America": 5, "South America": 2, "Europe": 5, "Africa": 3, "Asia": 7,
+                             "Australia": 2}
+        for continent, count in continent_counts.items():
+            if count == continent_requirements.get(continent, 0):
+                bonus += continent_bonuses.get(continent, 0)
+        return bonus
+
+    def find_player_territory_index(self, name):
+        for i, territory in enumerate(self.territories):
+            if territory.name == name:
+                return i
+        return None
+
 
 class Card:
     def __init__(self, army_type, territory):
         self.army_type = army_type  # string
         self.territory = territory  # string
+
 
 class Cards:
     def __init__(self):
@@ -30,9 +72,28 @@ class Cards:
             self.cards.append(Card(random_type, territory))
         self.cards.append(Card(Army("Infantry/Cavalry/Artillery", None), Territory("Wild Card", "WC", "")))
         self.cards.append(Card(Army("Infantry/Cavalry/Artillery", None), Territory("Wild Card", "WC", "")))
+        random.shuffle(self.cards)
 
     def get_cards(self):
         return self.cards
+
+
+class SecretMissionCards:
+    def __init__(self):
+        self.cards = [
+            "Capture Europe, Australia and one other continent",
+            "Capture Europe, South America and one other continent",
+            "Capture North America and Africa",
+            "Capture Asia and South America",
+            "Capture North America and Australia",
+            "Capture 24 territories",
+            "Destroy all armies of a named opponent or, in the case of being the named player oneself, to capture 24 territories",
+            "Capture 18 territories and occupy each with two troops"
+        ]
+
+        def get_cards(self):
+            return self.cards
+
 
 class Attack_dice:
     def __init__(self, num_one, num_two, num_three):
@@ -41,7 +102,7 @@ class Attack_dice:
         self.num_three = num_three  # int
 
     def roll_one_dice(self):
-        self.num_one = random.randint(1,8)
+        self.num_one = random.randint(1, 8)
         return self.num_one
 
     def roll_two_dice(self):
@@ -55,13 +116,14 @@ class Attack_dice:
         self.num_three = random.randint(1, 8)
         return self.num_one, self.num_two, self.num_three
 
+
 class Defend_dice:
     def __init__(self, num_one, num_two):
         self.num_one = num_one  # int
         self.num_two = num_two  # int
 
     def roll_one_dice(self):
-        self.num_one = random.randint(1,8)
+        self.num_one = random.randint(1, 8)
         return self.num_one
 
     def roll_two_dice(self):
@@ -69,61 +131,156 @@ class Defend_dice:
         self.num_two = random.randint(1, 8)
         return self.num_one, self.num_two
 
+
 class Territory:
-    def __init__(self, name, code, continent):
+    def __init__(self, name, code, continent, power, bordering=None):
         self.name = name
         self.code = code
         self.continent = continent
+        self.power = power
+        self.bordering = bordering if bordering is not None else []
+
+
+    def get_bordering_territories(self):
+        return self.bordering
+
+    def add_bordering_territory(self, territory):
+        self.bordering.append(territory)
+
 
 class Territories():
     def __init__(self):
         self.territories = [
-            Territory("Afghanistan", "AF", "Asia"),
-            Territory("Algeria", "DZ", "Africa"),
-            Territory("Angola", "AO", "Africa"),
-            Territory("Armenia", "AM", "Asia"),
-            Territory("Azerbaijan", "AZ", "Asia"),
-            Territory("Bangladesh", "BD", "Asia"),
-            Territory("Belarus", "BY", "Europe"),
-            Territory("Bolivia", "BO", "South America"),
-            Territory("Burma", "MM", "Asia"),
-            Territory("Burundi", "BI", "Africa"),
-            Territory("Central African Republic", "CF", "Africa"),
-            Territory("Chad", "TD", "Africa"),
-            Territory("Colombia", "CO", "South America"),
-            Territory("Democratic Republic of the Congo", "CD", "Africa"),
-            Territory("Ecuador", "EC", "South America"),
-            Territory("Egypt", "EG", "Africa"),
-            Territory("Eritrea", "ER", "Africa"),
-            Territory("Ethiopia", "ET", "Africa"),
-            Territory("Guinea", "GN", "Africa"),
-            Territory("Haiti", "HT", "North America"),
-            Territory("Iran", "IR", "Asia"),
-            Territory("Iraq", "IQ", "Asia"),
-            Territory("Jordan", "JO", "Asia"),
-            Territory("Kazakhstan", "KZ", "Asia"),
-            Territory("Kenya", "KE", "Africa"),
-            Territory("Lebanon", "LB", "Asia"),
-            Territory("Libya", "LY", "Africa"),
-            Territory("Mali", "ML", "Africa"),
-            Territory("Mauritania", "MR", "Africa"),
-            Territory("Moldova", "MD", "Europe"),
-            Territory("Nepal", "NP", "Asia"),
-            Territory("Nigeria", "NG", "Africa"),
-            Territory("North Korea", "KP", "Asia"),
-            Territory("Pakistan", "PK", "Asia"),
-            Territory("Russia", "RU", "Europe"),
-            Territory("Somalia", "SO", "Africa"),
-            Territory("South Sudan", "SS", "Africa"),
-            Territory("Sudan", "SD", "Africa"),
-            Territory("Syria", "SY", "Asia"),
-            Territory("Ukraine", "UA", "Europe"),
-            Territory("Venezuela", "VE", "South America"),
-            Territory("Yemen", "YE", "Asia"),
-            Territory("Zimbabwe", "ZW", "Africa")
+            # North America
+            Territory("Alaska", "AK", "North America", 0),
+            Territory("Alberta", "AB", "North America", 0),
+            Territory("Central America", "CA", "North America", 0),
+            Territory("Eastern United States", "US-E", "North America", 0),
+            Territory("Greenland", "GL", "North America", 0),
+            Territory("Northwest Territory", "NT", "North America", 0),
+            Territory("Ontario", "ON", "North America", 0),
+            Territory("Quebec", "QC", "North America", 0),
+            Territory("Western United States", "US-W", "North America", 0),
+
+            # South America
+            Territory("Argentina", "AR", "South America", 0),
+            Territory("Brazil", "BR", "South America", 0),
+            Territory("Peru", "PE", "South America", 0),
+            Territory("Venezuela", "VE", "South America", 0),
+
+            # Europe
+            Territory("Great Britain", "GB", "Europe", 0),
+            Territory("Iceland", "IS", "Europe", 0),
+            Territory("Northern Europe", "NE", "Europe", 0),
+            Territory("Scandinavia", "SC", "Europe", 0),
+            Territory("Southern Europe", "SE", "Europe", 0),
+            Territory("Ukraine", "UA", "Europe", 0),
+            Territory("Western Europe", "WE", "Europe", 0),
+
+            # Africa
+            Territory("Congo", "CG", "Africa", 0),
+            Territory("East Africa", "EA", "Africa", 0),
+            Territory("Egypt", "EG", "Africa", 0),
+            Territory("Madagascar", "MG", "Africa", 0),
+            Territory("North Africa", "NA", "Africa", 0),
+            Territory("South Africa", "ZA", "Africa", 0),
+
+            # Asia
+            Territory("Afghanistan", "AF", "Asia", 0),
+            Territory("China", "CN", "Asia", 0),
+            Territory("India", "IN", "Asia", 0),
+            Territory("Irkutsk", "IRK", "Asia", 0),
+            Territory("Japan", "JP", "Asia", 0),
+            Territory("Kamchatka", "KAM", "Asia", 0),
+            Territory("Middle East", "ME", "Asia", 0),
+            Territory("Mongolia", "MN", "Asia", 0),
+            Territory("Siam", "TH", "Asia", 0),
+            Territory("Siberia", "SB", "Asia", 0),
+            Territory("Ural", "UR", "Asia", 0),
+            Territory("Yakutsk", "YK", "Asia", 0),
+
+            # Australia
+            Territory("Eastern Australia", "EAU", "Australia", 0),
+            Territory("Indonesia", "ID", "Australia", 0),
+            Territory("New Guinea", "NG", "Australia", 0),
+            Territory("Western Australia", "WAU", "Australia", 0)
         ]
+
+        self.setup_borders()
+
+    def setup_borders(self):
+        borders = {
+            # North America
+            "Alaska": ["Alberta", "Northwest Territory"],
+            "Alberta": ["Alaska", "Northwest Territory", "Ontario"],
+            "Central America": ["Eastern United States", "Western United States"],
+            "Eastern United States": ["Central America", "Ontario", "Quebec"],
+            "Greenland": ["Northwest Territory", "Quebec"],
+            "Northwest Territory": ["Alaska", "Alberta", "Greenland"],
+            "Ontario": ["Alberta", "Eastern United States", "Quebec"],
+            "Quebec": ["Eastern United States", "Greenland", "Ontario"],
+            "Western United States": ["Central America", "Eastern United States"],
+
+            # South America
+            "Argentina": ["Brazil", "Peru"],
+            "Brazil": ["Argentina", "Peru", "Venezuela"],
+            "Peru": ["Argentina", "Brazil"],
+            "Venezuela": ["Brazil", "Peru"],
+
+            # Europe
+            "Great Britain": ["Iceland", "Northern Europe"],
+            "Iceland": ["Great Britain", "Scandinavia"],
+            "Northern Europe": ["Great Britain", "Scandinavia", "Western Europe"],
+            "Scandinavia": ["Iceland", "Northern Europe"],
+            "Southern Europe": ["Northern Europe", "Ukraine"],
+            "Ukraine": ["Northern Europe", "Southern Europe"],
+            "Western Europe": ["Northern Europe", "Southern Europe"],
+
+            # Africa
+            "Congo": ["East Africa", "North Africa"],
+            "East Africa": ["Congo", "Egypt", "North Africa"],
+            "Egypt": ["East Africa", "North Africa"],
+            "Madagascar": ["East Africa"],
+            "North Africa": ["Congo", "Egypt"],
+            "South Africa": ["East Africa"],
+
+            # Asia
+            "Afghanistan": ["China", "India"],
+            "China": ["Afghanistan", "India", "Mongolia"],
+            "India": ["Afghanistan", "China", "Siam"],
+            "Irkutsk": ["Mongolia", "Siberia"],
+            "Japan": ["Kamchatka"],
+            "Kamchatka": ["Japan", "Mongolia"],
+            "Middle East": ["India", "Ural"],
+            "Mongolia": ["China", "Kamchatka", "Siberia"],
+            "Siam": ["India"],
+            "Siberia": ["China", "Irkutsk", "Mongolia"],
+            "Ural": ["China", "Middle East"],
+            "Yakutsk": ["Siberia"],
+
+            # Australia
+            "Eastern Australia": ["New Guinea", "Western Australia"],
+            "Indonesia": ["New Guinea", "Western Australia"],
+            "New Guinea": ["Eastern Australia", "Indonesia"],
+            "Western Australia": ["Eastern Australia", "Indonesia"]
+        }
+        for territory in self.territories:
+            if territory.name in borders:
+                border_names = borders[territory.name]
+                for border_name in border_names:
+                    border_territory = self.find_territory_by_name(border_name)
+                    if border_territory:
+                        territory.add_bordering_territory(border_territory.name)
+
     def get_territories(self):
         return self.territories
+
+
+    def find_territory_by_name(self, name):
+        for territory in self.territories:
+            if territory.name.lower() == name.lower():
+                return territory
+        return None
 
     def get_territories_by_continent(self, continent):
         return [territory for territory in self.territories if territory.continent == continent]
@@ -134,17 +291,69 @@ class Territories():
                 return index
         return -1
 
+    def get_all_territories_names(self):
+        for territory in self.territories:
+            print(territory.name)
+
+class Dev_territories():
+    def __init__(self):
+        self.territories = [
+            Territory("Alaska", "AK", "North America", 0),
+            Territory("Argentina", "AR", "South America", 0),
+            Territory("Congo", "CG", "Africa", 0)
+        ]
+        self.setup_borders()
+
+    def setup_borders(self):
+        borders = {
+            "Alaska": ["Argentina", "Congo"],
+            "Argentina": ["Congo"],
+            "Congo": ["Argentina", "Alaska"]}
+
+        for territory in self.territories:
+            if territory.name in borders:
+                border_names = borders[territory.name]
+                for border_name in border_names:
+                    border_territory = self.find_territory_by_name(border_name)
+                    if border_territory:
+                        territory.add_bordering_territory(border_territory.name)
+
+    def get_territories(self):
+        return self.territories
+
+    def find_territory_by_name(self, name):
+        for territory in self.territories:
+            if territory.name.lower() == name.lower():
+                return territory
+        return None
+
+    def get_territories_by_continent(self, continent):
+        return [territory for territory in self.territories if territory.continent == continent]
+
+    def find_index_by_name(self, name):
+        for index, territory in enumerate(self.territories):
+            if territory.name.lower() == name.lower():
+                return index
+        return -1
+
+    def get_all_territories_names(self):
+        for territory in self.territories:
+            print(territory.name)
+
+
+
 class Army:
     def __init__(self, name, strength):
         self.name = name
         self.strength = strength
 
+
 class Army_types:
     def __init__(self):
         self.types = [
-        Army("Infantry", 1),
-        Army("Cavalry", 5),
-        Army("Artillery", 10)
+            Army("Infantry", 1),
+            Army("Cavalry", 5),
+            Army("Artillery", 10)
         ]
 
     def get_types(self):
